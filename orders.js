e3aefce9aa258157168d9965c07886374e9dcecd
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-07-07 15:24:18 +0100

    Update orders.js

--- orders.js
+++ orders.js
@@ -1,4 +1,3 @@
-// Firebase connection
 import { auth, db } from "./firebase.js";
 
 
@@ -65,31 +64,84 @@ onAuthStateChanged(auth, (user) => {
                 const order = doc.data();
 
 
+                let currencySymbol = "";
+
+                if(order.currency === "USD"){
+                    currencySymbol = "$";
+                }
+
+                if(order.currency === "NGN"){
+                    currencySymbol = "₦";
+                }
+
+
+
+                let statusClass = "pending";
+
+
+                if(order.status === "Approved"){
+                    statusClass = "approved";
+                }
+
+
+                if(order.status === "Rejected"){
+                    statusClass = "rejected";
+                }
+
+
 
                 ordersList.innerHTML += `
+
 
                 <div class="feature-card">
 
 
-                    <h3>${order.giftCard}</h3>
+                    <h3>
+                    🎁 ${order.giftCard}
+                    </h3>
 
 
                     <p>
-                    Amount: ${order.amount}
+                    🌍 Country: ${order.country}
                     </p>
 
 
                     <p>
+                    💵 Amount: ${currencySymbol}${order.amount}
+                    </p>
+
+
+                    <p>
+                    💳 Code: ${order.cardCode}
+                    </p>
+
+
+                    <p class="${statusClass}">
                     Status: ${order.status}
                     </p>
 
 
+
                     <p>
-                    Submitted by: ${order.userEmail}
+                    📸 Card Images:
                     </p>
 
 
+                    <a href="${order.frontImageURL}" target="_blank">
+                    View Front Image
+                    </a>
+
+
+                    <br>
+
+
+                    <a href="${order.backImageURL}" target="_blank">
+                    View Back Image
+                    </a>
+
+
                 </div>
+
 
                 `;
 
